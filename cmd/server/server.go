ef859de032db7288036fbaf5c41bf7fad39193fd
Gisli Hrafnkelsson <gisli@fastmail.com>
2021-11-14 11:58:23 +0000

    fix-cores

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -18,11 +18,19 @@ type config struct {
 	DatabaseURL string `envconfig:"DATABASE_URL" required:"true'`
 }
 
-func cors(next http.Handler) http.Handler {
+func cors(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		next.ServeHTTP(w, r)
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers")
+		origin := r.Header.Get("Origin")
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Methods", "GET,POST")
+
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token, Authorization")
+			return
+		} else {
+			h.ServeHTTP(w, r)
+		}
 	})
 }
 
