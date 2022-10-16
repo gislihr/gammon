1343b71dcbadf33b47919dc142ac09e015ba6c79
Gisli Hrafnkelsson <gisli@fastmail.com>
2022-10-16 13:53:13 +0000

    fix

--- pkg/gammon/db/store.go
+++ pkg/gammon/db/store.go
@@ -189,7 +189,7 @@ func (s *Store) GetPlayers(pr PlayerRequest) ([]*model.Player, error) {
 func (s *Store) GetGames(gr GameRequest) ([]*internalModel.Game, error) {
 	psql := sq.StatementBuilder.PlaceholderFormat(sq.Dollar)
 	queryBuilder := psql.Select(gameFields...).From("game").Limit(uint64(gr.Limit)).Offset(uint64(gr.Offset)).OrderBy("id desc")
-	queryBuilder = queryBuilder.OrderBy("date asc")
+	queryBuilder = queryBuilder.OrderBy("date desc")
 
 	or := sq.Or{}
 
